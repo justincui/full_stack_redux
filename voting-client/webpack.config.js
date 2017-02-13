55c3be33a1686a45099c0be7eb72105e1b7b3688
Justin Cui <justincui@gmail.com>
2017-02-12 23:54:50 -0800

    today ends

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 exclude:/node_modules/,
                 loader: 'babel',
             },
-        ]
+        ],
     },
     resolve:{
         extensions:['', '.js', '.jsx'],
