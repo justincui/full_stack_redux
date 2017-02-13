f157546f7ef0157a05ed637044b8863162d90378
Justin Cui <justincui@gmail.com>
2017-02-13 14:20:04 -0800

    webpack compiling ok

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,5 +1,9 @@
+/* voting-client */
+var webpack = require('webpack');
 module.exports = {
     entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
         './src/index.js',
     ],
     output: {
@@ -12,14 +16,18 @@ module.exports = {
             {
                 test:/\.jsx?$/,
                 exclude:/node_modules/,
-                loader: 'babel',
+                loader: 'react-hot-loader!babel-loader',
             },
         ],
     },
     resolve:{
-        extensions:['', '.js', '.jsx'],
+        extensions: ['*', '.js', '.jsx'],
     },
     devServer: {
         contentBase: './dist',
+        hot:true,
     },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 };
