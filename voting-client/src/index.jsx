bb08a377afa0231c0c5c9c07c3ba14b12d5a88cc
Justin Cui <justincui@gmail.com>
2017-02-15 18:48:37 -0800

    update

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -5,6 +5,8 @@ import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
+import io from 'socket.io-client';
+
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
@@ -20,6 +22,8 @@ store.dispatch({
         },
     },
 });
+
+const socket=io(`${location.protocol}//${location.hostname}:8090`);
 
 const routes = <Route component={App}>
     <Route path="/results" component={ResultsContainer}/>
